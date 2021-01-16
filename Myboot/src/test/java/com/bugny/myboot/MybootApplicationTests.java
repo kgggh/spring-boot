7a2bd1cff51693422f1b767c25e1a63e7446c515
kgggh <68227318+kgggh@users.noreply.github.com>
2021-01-16 21:57:12 +0900

    spring boot start!!!!!!!

---
+++ Myboot/src/test/java/com/bugny/myboot/MybootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bugny.myboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
